2ba1bc41ae85f8faff711708b4bf0cbcbd790818
Andrew Smirnov <citizen.andrew@yandex.ru>
2022-11-13 21:33:27 +0300

    Рабочий коммит

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,34 @@
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import post
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
-def group_posts(request, post_slug):
-    return HttpResponse(f'Посты групп {post_slug}')
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
